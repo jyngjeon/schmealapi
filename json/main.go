52eef69622b0aff1fa7f6c05050e7ec4c8a12644
Jeyoung Jeon <jeyoung@Appleui-MacBookPro.local>
2019-03-11 20:39:14 +0900

    siri ver. of []string

---
+++ json/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/anaskhan96/soup"
+	"github.com/gorilla/mux"
+)
+
+//SchoolMeal is struct of schmeal for output(GET)
+type SchoolMeal struct {
+	Brkf []string
+	Lnch []string
+	Dinr []string
+	Snck []string
+	err  string
+}
+
+var schmeal []SchoolMeal
+
+//GetMeal parses the meal info from school website
+func GetMeal() []soup.Root {
+	resp, err := soup.Get("http://hana.hs.kr/life/meal.asp?yy=2018&mm=11")
+	fmt.Println("http transport error is:", err)
+
+	doc := soup.HTMLParse(resp)
+	meal := doc.Find("table", "class", "today_meal").FindAll("td")
+
+	return meal
+}
+
+//WriteMeal makes the struct
+func WriteMeal() SchoolMeal {
+	meal := GetMeal()
+	sch := SchoolMeal{}
+	if len(meal) > 1 {
+		brkf := strings.Split(meal[0].Text(), ",")
+		lnch := strings.Split(meal[1].Text(), ",")
+		dinr := strings.Split(meal[2].Text(), ",")
+		snck := strings.Split(meal[3].Text(), ",")
+
+		sch.Brkf = brkf
+		sch.Lnch = lnch
+		sch.Dinr = dinr
+		sch.Snck = snck
+	} else {
+		sch.err = meal[0].Text()
+	}
+	return sch
+}
+
+//SendMeal is for api work
+func SendMeal(w http.ResponseWriter, r *http.Request) {
+	schmeal := WriteMeal()
+	json.NewEncoder(w).Encode(schmeal)
+}
+
+// our main function
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/schmeal", SendMeal).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
---
+++ siri/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/anaskhan96/soup"
+	"github.com/gorilla/mux"
+)
+
+//var for siri (to read Korean only)
+var sch [4]string
+
+//GetMeal parses the meal info from school website
+func GetMeal() []soup.Root {
+	resp, err := soup.Get("http://hana.hs.kr/life/meal.asp?yy=2018&mm=11")
+	fmt.Println("http transport error is:", err)
+
+	doc := soup.HTMLParse(resp)
+	meal := doc.Find("table", "class", "today_meal").FindAll("td")
+
+	return meal
+}
+
+//WriteMeal makes the struct
+func WriteMeal() [4]string {
+	meal := GetMeal()
+	if len(meal) > 1 {
+		brkf := strings.Split(meal[0].Text(), ",")
+		lnch := strings.Split(meal[1].Text(), ",")
+		dinr := strings.Split(meal[2].Text(), ",")
+		snck := strings.Split(meal[3].Text(), ",")
+
+		sch[0] = strings.Join(brkf, ",")
+		sch[1] = strings.Join(lnch, ",")
+		sch[2] = strings.Join(dinr, ",")
+		sch[3] = strings.Join(snck, ",")
+	} else {
+		sch[0] = meal[0].Text()
+	}
+	return sch
+}
+
+//SendMeal for api work
+func SendMeal(w http.ResponseWriter, r *http.Request) {
+	schmeal := WriteMeal()
+	json.NewEncoder(w).Encode(schmeal)
+}
+
+// our main function
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/schmeal", SendMeal).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
